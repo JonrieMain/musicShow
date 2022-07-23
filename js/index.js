10b27a510285dc413e087beaefef8f6b11bbfd54
Jonrie Morales Badiang <68489573+JonrieMain@users.noreply.github.com>
2022-07-23 08:18:10 +0800

    reduce image sizes & add some js

--- js/index.js
+++ js/index.js
@@ -1,23 +1,53 @@
-const nav = document.querySelector('.nav');
-const navSmallWidth = document.querySelector('.navSmallWidth');
-navSmallWidth.addEventListener('click', ()=>{
-	if(navSmallWidth.classList.contains('open')){
-		/* change nav style to open*/
-		nav.style.width="60%";
-		/* change src */
-		navSmallWidth.src="./img/close.png"
+	const header = document.querySelector('#header')
+	window.addEventListener('scroll', () =>{
+		if(window.matchMedia("(min-width: 826px)").matches){
+			header.classList.toggle('sticky', window.scrollY > 0)
+		}
+	});
 
-		/* remove classList and add*/
-		navSmallWidth.classList.remove('open');
-		navSmallWidth.classList.add('close');
-	}else{
-		/* change nav style to open*/
-		nav.style.width="0%";
-		/* change src */
-		navSmallWidth.src="./img/open.png"
 
-		/* remove classList and add*/
-		navSmallWidth.classList.add('open');
-		navSmallWidth.classList.remove('close');
-	}
-});
+
+
+	const nav = document.querySelector('.nav');
+	const navSmallWidth = document.querySelector('.navSmallWidth');
+	navSmallWidth.addEventListener('click', ()=>{
+		if(navSmallWidth.classList.contains('open')){
+			/* change nav style to open*/
+			nav.style.width="70%";
+			/* change src */
+			navSmallWidth.src="./img/close.png"
+
+			/* remove classList and add*/
+			navSmallWidth.classList.remove('open');
+			navSmallWidth.classList.add('close');
+		}else{
+			/* change nav style to open*/
+			nav.style.width="0%";
+			/* change src */
+			navSmallWidth.src="./img/open.png"
+
+			/* remove classList and add*/
+			navSmallWidth.classList.add('open');
+			navSmallWidth.classList.remove('close');
+		}
+	});
+
+
+
+
+	const ac = document.querySelectorAll('.ac')
+
+		/* click active check*/
+		ac.forEach(actives =>{
+			actives.addEventListener('click',()=>{
+
+				/* remove */
+				ac.forEach(re =>{
+					re.classList.remove('active')
+				})
+
+				/* add */
+				actives.classList.add('active')
+			})
+		})
+	
